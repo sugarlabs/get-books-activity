ccc7f9e671ad6704718ab89bb2bda484a439523f
Sayamindu Dasgupta <sayamindu@gmail.com>
2009-10-15 20:49:38 +0530

    Add languagenames.py: To decode ISO 639 language codes

---
+++ languagenames.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python
+
+# Copyright (C) 2009 Sayamindu Dasgupta <sayamindu@laptop.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+import libxml2
+
+_ISO_639_XML_PATH = '/usr/share/xml/iso-codes/iso_639.xml'
+
+
+
+class LanguageNames(object):
+    def __init__(self):
+        try:
+            self._xmldoc = libxml2.parseFile('/usr/share/xml/iso-codes/iso_639.xml')
+        except libxml2.parserError:
+            self._xmldoc = None
+            return
+
+        self._eroot = self._xmldoc.firstElementChild()
+
+    def close(self):
+        if self._xmldoc is not None:
+            self._xmldoc.free()
+
+    def get_full_language_name(self, code):
+        if self._xmldoc is None:
+            return code
+
+        for child in self._eroot.children:
+            if child.properties is not None:
+                for property in child.properties:
+                    if property.get_content() == code:
+                        return self._real_get_full_language_name(child)
+
+        return code
+
+    def _real_get_full_language_name(self, node):
+        for property in node.properties:
+            if property.get_name() == 'name':
+                return property.get_content()
+
