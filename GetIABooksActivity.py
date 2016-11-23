ac262316035aeee492f0ac9b04f0cbb8fa430a37
James Cameron <quozl@laptop.org>
2016-11-23 11:44:39 +1100

    Fix warnings

--- GetIABooksActivity.py
+++ GetIABooksActivity.py
@@ -20,6 +20,9 @@ import os
 import logging
 import time
 
+import gi
+gi.require_version('Gtk', '3.0')
+
 from gi.repository import Gtk
 from gi.repository import Gdk
 from gi.repository import GdkPixbuf
